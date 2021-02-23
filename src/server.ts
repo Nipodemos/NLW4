1442f301e5b94abd915e5d048140ab24faf65546
Nipodemos <nipodemos13@gmail.com>
2021-02-23 01:19:01 -0300

    feat: :sparkles: aula 1 terminada

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 
-
 const app = express();
 
-
-
-
+app.get('/users', (request, response) => response.send({ message: 'Hello World - NLW4' }));
+app.post('/', (request, response) => response.json({ message: 'Os dados foram salvos com sucesso' }));
 app.listen(3333, () => {
-  console.log("Servidor aberto em http://localhost:3333");
-})
+  console.log('Servidor aberto em http://localhost:3333');
+});
